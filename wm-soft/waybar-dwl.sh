8366b62958da7605a936cc334fda93d9ec81548c
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-08-05 22:52:02 +0200

    switch fancy waybar-dwl

--- wm-soft/waybar-dwl.sh
+++ wm-soft/waybar-dwl.sh
@@ -4,172 +4,131 @@
 #   Based heavily upon this script by user "novakane" (Hugo Machet) used to do the same for yambar
 #   https://codeberg.org/novakane/yambar/src/branch/master/examples/scripts/dwl-tags.sh
 #
-# USAGE: waybar-dwl.sh MONITOR COMPONENT
-#        "COMPONENT" is an integer representing a dwl tag OR "layout" OR "title"
+# USAGE: waybar-dwl.sh MONITOR
+#        "MONITOR"   is a wayland output such as "eDP-1"
+#
+# STYLE: Unlike "normal" waybar modules, styles (foreground, background, underline, overline) are NOT set in style.css
+#        Instead, modify the pango_* variables in THIS file
+#        A guide to available pango span styling can be found here: https://docs.gtk.org/Pango/pango_markup.html
+#
+# SELMON INDICATION: on multiple output dwl setups, the inactve monitor will display most elements in the
+#                    "inactive" pango style selected. This allows the user to glance at the waybar instance
+#                    and be immediately aware of which is the active output (monitor).
 #
 # REQUIREMENTS:
 #  - inotifywait ( 'inotify-tools' on arch )
-#  - Launch dwl with `dwl > ~.cache/dwltags` or change $fname
+#  - Launch dwl with `dwl > ~.cache/dwltags` or change $dwl_output_filename
 #
 # Now the fun part
 #
-### Example ~/.config/waybar/config
+### Example (multi-monitor) ~/.config/waybar/config
+#          - waybar-dwl.sh takes one argument: the monitor id as returned by wlr-randr -- e.g. "eDP-1"
+#          - naming the module with the monitor id as a #suffix allows running multiple instances of the
+#                  dwl module in, perhaps, a waybar instance on each monitor
+# [
+#     {
+#     "output":        "DP-2",
+#     "modules-left":  ["custom/dwl#DP-2"],
+#     "modules-right": ["clock"],
+#     "custom/dwl#DP-2": {
+# 	"exec":                    "/xap/etc/waybar/waybar-dwl.sh 'DP-2'",
+# 	"format":                  "{}",
+# 	"return-type":             "json"
+#     },
+# },
+#     {
+#     "output":        "HDMI-A-1",
+#     "modules-left":  ["custom/dwl#HDMI-A-1"],
+#     "modules-right": ["clock"],
+#     "custom/dwl#HDMI-A-1": {
+# 	"exec":                    "/xap/etc/waybar/waybar-dwl.sh 'HDMI-A-1'",
+# 	"format":                  "{}",
+# 	"return-type":             "json"
+#     },
+# },
+# ]
 #
-# {
-#   "modules-left": ["custom/dwl_tag#0", "custom/dwl_tag#1", "custom/dwl_tag#2", "custom/dwl_tag#3", "custom/dwl_tag#4", "custom/dwl_tag#5", "custom/dwl_layout", "custom/dwl_title"],
-#   // The empty '' argument used in the following "exec": fields works for single-monitor setups
-#   // For multi-monitor setups, see https://github.com/Alexays/Waybar/wiki/Configuration
-#   //     and enter the monitor id (like "eDP-1") as the first argument to waybar-dwl.sh
-#   "custom/dwl_tag#0": {
-#     "exec": "/path/to/waybar-dwl.sh '' 0",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#1": {
-#     "exec": "/path/to/waybar-dwl.sh '' 1",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#2": {
-#     "exec": "/path/to/waybar-dwl.sh '' 2",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#3": {
-#     "exec": "/path/to/waybar-dwl.sh '' 3",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#4": {
-#     "exec": "/path/to/waybar-dwl.sh '' 4",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#5": {
-#     "exec": "/path/to/waybar-dwl.sh '' 5",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#6": {
-#     "exec": "/path/to/waybar-dwl.sh '' 6",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#7": {
-#     "exec": "/path/to/waybar-dwl.sh '' 7",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#8": {
-#     "exec": "/path/to/waybar-dwl.sh '' 8",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_tag#9": {
-#     "exec": "/path/to/waybar-dwl.sh '' 9",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_layout": {
-#     "exec": "/path/to/waybar-dwl.sh '' layout",
-#     "format": "{}",
-#     "return-type": "json"
-#   },
-#   "custom/dwl_title": {
-#     "exec": "/path/to/waybar-dwl.sh '' title",
-#     "format": "{}",
-#     "escape": true,
-#     "return-type": "json"
-#   }
-# }
-#
-### Example ~/.config/waybar/style.css
-# #custom-dwl_layout {
-#     color: #EC5800
-# }
-#
-# #custom-dwl_title {
-#     color: #017AFF
-# }
-#
-# #custom-dwl_tag {
-#     color: #875F00
-# }
-#
-# #custom-dwl_tag.selected {
-#     color: #017AFF
-# }
-#
-# #custom-dwl_tag.urgent {
-#     background-color: #FF0000
-# }
-#
-# #custom-dwl_tag.active {
-#     border-top: 1px solid #EC5800
-# }
 
-# Variables
-declare output title layout activetags selectedtags
-declare -a tags name
-readonly fname="$HOME"/.cache/dwltags
 
-tags=( "1" "2" "3" "4" "5" "6" "7" "8" "9" )
-name=( "1" "2" "3" "4" "5" "6" "7" "8" "9" ) # Array of labels for tags
 
+############### USER: MODIFY THESE VARIABLES ###############
+readonly dwl_output_filename="$HOME"/.cache/dwltags                  # File to watch for dwl output
+readonly labels=( "" "" "" "" "" "" "" "" "")              # Number of lables must match dwl's config.h tagcount
+# readonly labels=( "a" "r" "s" "t" "d" "g" "p" "b"  )              # Number of lables must match dwl's config.h tagcount
+max_title_length=70                                                  # Adjust according to available space in YOUR waybar - prevent right-side overflow
+pango_tag_default="<span                      foreground='#00ffff'>" # Pango span style for 'default' tags
+# pango_tag_active="<span overline='single' overline_color='#a020f0'>" # Pango span style for 'active' tags
+pango_tag_active="<span                       foreground='#a6cc70'>" # Pango span style for 'active' tags
+pango_tag_selected="<span                     foreground='#ff00ff'>" # Pango span style for 'selected' tags
+pango_tag_urgent="<span                       background='#ed8274'>" # Pango span style for 'urgent' tags
+pango_layout="<span                           foreground='#fe8019'>" # Pango span style for 'layout' character
+pango_title="<span                            foreground='#458588'>" # Pango span style for 'title' monitor
+pango_inactive="<span                         foreground='#928374'>" # Pango span style for elements on an INACTIVE monitor
+############### USER: MODIFY THESE VARIABLES ###############
+
+dwl_log_lines_per_focus_change=7 # This has changed several times as dwl has developed and may not yet be rock solid
+full_components_list=( `seq 0 $(( ${#labels[@]} - 1 ))` "layout" "title" ) # (1, 2, ... length_of_$labels) + "layout" + "title"
 monitor="${1}"
-component="${2}"
 
 _cycle() {
-    case "${component}" in
-	# I only use six tags; if you use more, expand this array appropriately
-	[012345678])
-	    this_tag="${component}"
-	    unset this_status
-	    mask=$((1<<this_tag))
+    output_text=""
+    # Render some components in $pango_inactive if $monitor is not the active monitor
+    if [[ "${selmon}" = 0 ]]; then
+	local pango_tag_default="${pango_inactive}"
+	local pango_layout="${pango_inactive}"
+	local pango_title="${pango_inactive}"
+    fi
 
-	    if (( "${activetags}"   & mask )) 2>/dev/null; then this_status+='"active",'  ; fi
-	    if (( "${selectedtags}" & mask )) 2>/dev/null; then this_status+='"selected",'; fi
-	    if (( "${urgenttags}"   & mask )) 2>/dev/null; then this_status+='"urgent",'  ; fi
+    for component in "${full_components_list[@]}"; do
+	case "${component}" in
+	    # If you use fewer than 9 tags, reduce this array accordingly
+	    [012345678])
+		mask=$((1<<component))
+		tag_text=${labels[component]}
+		# Wrap component in the applicable nestable pango spans
+                if (( "${urgenttags}"   & mask )) 2>/dev/null; then tag_text="${pango_tag_urgent}${tag_text}</span>"; fi
+                if (( "${selectedtags}" & mask )) 2>/dev/null; then tag_text="${pango_tag_selected}${tag_text}</span>"
+                elif (( "${activetags}"   & mask )) 2>/dev/null; then tag_text="${pango_tag_active}${tag_text}</span>";
+		else
+			tag_text="${pango_tag_default}${tag_text}</span>"
+		fi
 
-	    if [[ "${this_status}" ]]; then
-		printf -- '{"text":" %s ","class":[%s]}\n' "${name[this_tag]}" "${this_status}"
-	    else
-		printf -- '{"text":"","class":["empty"]}\n'
-	    fi
-	    ;;
-	layout)
-	    printf -- '{"text":"%s"}\n' "${layout}"
-	    ;;
-	title)
-	    printf -- '{"text":"%s"}\n' "${title}"
-	    ;;
-	*)
-	    printf -- '{"text":"INVALID INPUT"}\n'
-	    ;;
-    esac
+		output_text+="${tag_text}  "
+		;;
+	    layout)
+		    output_text+="${pango_layout}${layout} </span>"
+		;;
+	    title)
+		    output_text+="${pango_title}${title}</span>"
+		;;
+	    *)
+		output_text+="?" # If a "?" is visible on this module, something happened that shouldn't have happened
+		;;
+	esac
+    done
 }
 
 while [[ -n "$(pgrep waybar)" ]] ; do
-
-    [[ ! -f "${fname}" ]] && printf -- '%s\n' \
+    [[ ! -f "${dwl_output_filename}" ]] && printf -- '%s\n' \
 				    "You need to redirect dwl stdout to ~/.cache/dwltags" >&2
 
     # Get info from the file
-    output="$(grep  "${monitor}" "${fname}" | tail -n7)"
-    title="$(echo   "${output}" | grep '^[[:graph:]]* title'  | cut -d ' ' -f 3-  | sed s/\"/“/g )" # Replace quotes - prevent waybar crash
-    layout="$(echo  "${output}" | grep '^[[:graph:]]* layout' | cut -d ' ' -f 3- )"
-    #selmon="$(echo "${output}" | grep 'selmon')"
+    dwl_latest_output_by_monitor="$(grep  "${monitor}" "${dwl_output_filename}" | tail -n${dwl_log_lines_per_focus_change})"
+    title="$(echo   "${dwl_latest_output_by_monitor}" | grep '^[[:graph:]]* title'  | cut -d ' ' -f 3- )"
+    title="${title//\"/“}" # Replace quotation - prevent waybar crash
+    title="${title//\&/+}" # Replace ampersand - prevent waybar crash
+    title="${title::$max_title_length}" # Prevent waybar right-side overflow
+    layout="$(echo  "${dwl_latest_output_by_monitor}" | grep '^[[:graph:]]* layout' | cut -d ' ' -f 3- )"
+    selmon="$(echo  "${dwl_latest_output_by_monitor}" | grep 'selmon' | cut -d ' ' -f 3)"
 
     # Get the tag bit mask as a decimal
-    activetags="$(echo "${output}"   | grep '^[[:graph:]]* tags' | awk '{print $3}')"
-    selectedtags="$(echo "${output}" | grep '^[[:graph:]]* tags' | awk '{print $4}')"
-    urgenttags="$(echo "${output}"   | grep '^[[:graph:]]* tags' | awk '{print $6}')"
+    activetags="$(  echo "${dwl_latest_output_by_monitor}" | grep '^[[:graph:]]* tags' | awk '{print $3}')"
+    selectedtags="$(echo "${dwl_latest_output_by_monitor}" | grep '^[[:graph:]]* tags' | awk '{print $4}')"
+    urgenttags="$(  echo "${dwl_latest_output_by_monitor}" | grep '^[[:graph:]]* tags' | awk '{print $6}')"
 
     _cycle
+    printf -- '{"text":"%s"}\n' "${output_text}"
 
     # 60-second timeout keeps this from becoming a zombified process when waybar is no longer running
-    inotifywait -t 60 -qq --event modify "${fname}"
-
+    inotifywait -t 60 -qq --event modify "${dwl_output_filename}"
 done
-
-unset -v activetags layout name output selectedtags tags title
