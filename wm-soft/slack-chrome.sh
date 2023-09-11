79bd9eec00ae6f4304124b4b1cd4adf84aeba625
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-09-11 23:59:21 +0200

    slack/ff screensharing

---
+++ wm-soft/slack-chrome.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+google-chrome-beta --enable-features=UseOzonePlatform --ozone-platform=wayland --profile-directory='Profile 3' --new-window 'https://app.slack.com/client/T5RR4CT8U/C04FEE1AJLA'
