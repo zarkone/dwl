ccd0dbdb5266e2a15851d4df9838fcfdeff9c9b8
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-10-20 15:55:14 +0200

    adjust browser vars

--- wm-soft/firefox.sh
+++ wm-soft/firefox.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-MOZ_ENABLE_WAYLAND=1 firefox
+MOZ_ENABLE_WAYLAND=1 firefox-beta
--- wm-soft/slack-chrome.sh
+++ wm-soft/slack-chrome.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-google-chrome-beta --enable-features=UseOzonePlatform --ozone-platform=wayland --profile-directory='Profile 3' --new-window 'https://app.slack.com/client/T5RR4CT8U/C04FEE1AJLA'
+google-chrome-stable --enable-features=UseOzonePlatform --ozone-platform=wayland --profile-directory='Profile 2' --new-window 'https://app.slack.com/client/T5RR4CT8U/C04FEE1AJLA'
